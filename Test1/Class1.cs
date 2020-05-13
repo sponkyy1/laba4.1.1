e8580e4ea40daf84a43adf1aef8751f6c38443cb
OopLaba4.1 <OopLaba4_1@gmail.com>
2020-05-13 23:13:39 +0300

    first commit

---
+++ Test1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using Test1;
+using System;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student();
+            Student student2 = new Student();
+            Student student3 = new Student();
+            student1.setRating(99);
+            student2.setRating(80);
+            student3.setRating(51);
+            Console.Write("Student 1: ");
+            Console.WriteLine(student1.studentRating(student1.getRating()));
+            Console.Write("Student 2: ");
+            Console.WriteLine(student2.studentRating(student2.getRating()));
+            Console.Write("Student 3: ");
+            Console.WriteLine(student3.studentRating(student3.getRating()));
+        }
+    }
+
+}
---
+++ Test1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Test1
+{
+    class Student
+    {
+        private string lastName;
+        private string group;
+        private int year;
+        private string address;
+        private int passportId;
+        private int age;
+        private int telephoneNumber;
+        private int rating;
+        public Student() { }
+
+        public void setRating(int rating)
+        {
+            this.rating = rating;
+        }
+
+        public int getRating()
+        {
+            return this.rating;
+        }
+        public int Year { get => year; set => year = value; }
+        public string Address { get => address; set => address = value; }
+        public string Group { get => group; set => group = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public int PassportId { get => passportId; set => passportId = value; }
+        public int Age { get => age; set => age = value; }
+        public int TelephoneNumber { get => telephoneNumber; set => telephoneNumber = value; }
+        public string studentRating(int rating)
+        {
+            if (rating >= 90)
+            {
+                return "Greet the excellent student";
+            }
+            else if (rating >= 75)
+            {
+                return "You can study better";
+
+            }
+            else
+            {
+                return "51 i to dobre";
+            }
+        }
+    }
+}
